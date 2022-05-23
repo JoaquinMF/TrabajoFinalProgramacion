f8ca4d1ddaa5db699ef0f56e9863305253ecb72e
Joaquin <kino_1992@hotmail.com>
2022-05-23 16:13:33 +0100

    commit2

--- ProyectoFinalProgramacion/src/main/java/clases/NamedEntity.java
+++ ProyectoFinalProgramacion/src/main/java/clases/NamedEntity.java
@@ -1,5 +1,22 @@
 package clases;
 
 public class NamedEntity {
+	
+	private String name;
+
+	public NamedEntity(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 
 }
--- ProyectoFinalProgramacion/src/main/java/clases/Quest.java
+++ ProyectoFinalProgramacion/src/main/java/clases/Quest.java
@@ -1,5 +1,40 @@
 package clases;
 
-public class Quest {
+public class Quest extends NamedEntity{
+
+	
+	private Item reward;
+	private String description;
+	
+	
+	public Quest(String name, Item reward, String description) {
+		super(name);
+		this.reward = reward;
+		this.description = description;
+	}
+
+
+	public Item getReward() {
+		return reward;
+	}
+
+
+	public void setReward(Item reward) {
+		this.reward = reward;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
+	
 
 }
