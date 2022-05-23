2dddf8b6cd34693319a8c0d9a3693b69b5f73298
Joaquin <kino_1992@hotmail.com>
2022-05-23 16:49:19 +0100

    commit3

--- ProyectoFinalProgramacion/src/main/java/clases/NPC.java
+++ ProyectoFinalProgramacion/src/main/java/clases/NPC.java
@@ -1,5 +1,89 @@
 package clases;
 
-public class NPC {
+import java.util.ArrayList;
+
+import javax.swing.plaf.synth.Region;
+
+public class NPC extends NamedEntity {
+	
+	private Region location;
+	private boolean enemy;
+	private Quest give;
+	private boolean boss;
+	private ArrayList<Gear> loot;
+	private short hp;
+	private short sp;
+	private short strength;
+	private short stamina;
+	
+	public NPC(String name, Region location, boolean enemy, Quest give, boolean boss, ArrayList<Gear> loot, short hp,
+			short mp) {
+		super(name);
+		this.location = location;
+		this.enemy = enemy;
+		this.give = give;
+		this.boss = boss;
+		this.loot = loot;
+		this.hp = hp;
+		this.mp = mp;
+	}
+
+	public Region getLocation() {
+		return location;
+	}
+
+	public void setLocation(Region location) {
+		this.location = location;
+	}
+
+	public boolean isEnemy() {
+		return enemy;
+	}
+
+	public void setEnemy(boolean enemy) {
+		this.enemy = enemy;
+	}
+
+	public Quest getGive() {
+		return give;
+	}
+
+	public void setGive(Quest give) {
+		this.give = give;
+	}
+
+	public boolean isBoss() {
+		return boss;
+	}
+
+	public void setBoss(boolean boss) {
+		this.boss = boss;
+	}
+
+	public ArrayList<Gear> getLoot() {
+		return loot;
+	}
+
+	public void setLoot(ArrayList<Gear> loot) {
+		this.loot = loot;
+	}
+
+	public short getHp() {
+		return hp;
+	}
+
+	public void setHp(short hp) {
+		this.hp = hp;
+	}
+
+	public short getMp() {
+		return mp;
+	}
+
+	public void setMp(short mp) {
+		this.mp = mp;
+	}
+	
+	
 
 }
--- ProyectoFinalProgramacion/src/main/java/clases/World.java
+++ ProyectoFinalProgramacion/src/main/java/clases/World.java
@@ -1,5 +1,46 @@
 package clases;
 
-public class World {
+import java.util.ArrayList;
+import java.util.HashMap;
 
+public class World extends NamedEntity {
+	
+	private ArrayList<Quest> mainQuest;
+	//private collections sideQuest;
+	private GameWindow worldMap;
+	private HashMap<String, GameWindow> region;
+	
+	public World(String name, ArrayList<Quest> mainQuest, GameWindow worldMap, HashMap<String, GameWindow> region) {
+		super(name);
+		this.mainQuest = mainQuest;
+		this.worldMap = worldMap;
+		this.region = region;
+	}
+
+	public ArrayList<Quest> getMainQuest() {
+		return mainQuest;
+	}
+
+	public void setMainQuest(ArrayList<Quest> mainQuest) {
+		this.mainQuest = mainQuest;
+	}
+
+	public GameWindow getWorldMap() {
+		return worldMap;
+	}
+
+	public void setWorldMap(GameWindow worldMap) {
+		this.worldMap = worldMap;
+	}
+
+	public HashMap<String, GameWindow> getRegion() {
+		return region;
+	}
+
+	public void setRegion(HashMap<String, GameWindow> region) {
+		this.region = region;
+	}
+	
+	
+	
 }
