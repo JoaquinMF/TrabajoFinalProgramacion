3fc117af76d5ab2eeea259ffa2dfcfc03c79ef32
Joaquin <kino_1992@hotmail.com>
2022-05-24 18:48:56 +0100

    commit20

--- ProyectoFinalProgramacion/src/main/java/pantallas/UI.java
+++ ProyectoFinalProgramacion/src/main/java/pantallas/UI.java
@@ -1,5 +1,6 @@
 package pantallas;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -26,6 +27,7 @@ public class UI {
 		
 		createMainField();
 		createBackground();
+		createObject();
 		
 		window.setVisible(true);
 	}
@@ -33,20 +35,20 @@ public class UI {
 	public void createMainField() {
 		
 		window=new JFrame();
-		window.setSize(1024,800);
+		window.setSize(1100,960);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.getContentPane().setBackground(Color.black);
 		window.getContentPane().setLayout(null);
 		
 		
 		messages = new JTextArea("TEXTO DE PRUEBA");
-		messages.setBounds(10,588,988,162);
+		messages.setBounds(100,795,528,106);
 		messages.setBackground(new Color(135, 206, 235));
 		messages.setForeground(new Color(0, 0, 0));
 		messages.setEditable(false);
 		messages.setLineWrap(true);
 		messages.setWrapStyleWord(true);
-		messages.setFont(new Font("Gabriola", Font.PLAIN, 34));
+		messages.setFont(new Font("Gabriola", Font.PLAIN, 24));
 		window.getContentPane().add(messages);
 		
 	}
@@ -54,13 +56,31 @@ public class UI {
 	public void createBackground() {
 		
 		bgPanel[1]=new JPanel();
-		bgPanel[1].setBounds(50,50,1024,800);
-		bgPanel[1].setBackground(Color.blue);
+		bgPanel[1].setBounds(50,50,1100,960);
+		bgPanel[1].setBackground(Color.black);
 		bgPanel[1].setLayout(null);
-		window.add(bgPanel[1]);
+		window.getContentPane().add(bgPanel[1]);
 		
 		bgLabel[1]=new JLabel();
-		bgLabel[1].setBounds(0,0,1024,800);
+		bgLabel[1].setBounds(0,0,1100,960);
+		
+		ImageIcon bgIcon = new ImageIcon(getClass().getClassLoader().getResource("brightfoot1024x800.PNG"));
+		bgLabel[1].setIcon(bgIcon);
+		
+		
+		
+	}
+	
+	public void createObject() {
+		
+		JLabel objectLabel=new JLabel();
+		objectLabel.setBounds(750,690,200,84);
+		
+		ImageIcon objectIcon=new ImageIcon(getClass().getClassLoader().getResource("chest200x84OBJECT.png"));
+		objectLabel.setIcon(objectIcon);
+		
+		bgPanel[1].add(objectLabel);
+		bgPanel[1].add(bgLabel[1]);
 		
 		
 		
