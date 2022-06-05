d3357065441c6c11de425d177dd9b3e5643219dc
Joaquin <kino_1992@hotmail.com>
2022-06-05 16:44:38 +0100

    a

---
+++ src/main/java/principal/Main.java
@@ -0,0 +1,15 @@
+package principal;
+
+import java.util.ArrayList;
+
+import clases.GameManager;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		new GameManager();
+
+	}
+
+}
